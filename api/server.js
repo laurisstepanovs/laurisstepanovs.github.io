605fe25d338da76ededda387a441caa03b40bfcb
laurisstepanovs <33573735+laurisstepanovs@users.noreply.github.com>
2022-09-14 00:57:51 +0300

    feat: added sent email api

---
+++ api/server.js
@@ -0,0 +1,44 @@
+var express = require('express'),
+path = require('path'),
+nodeMailer = require('nodemailer'),
+bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json()); 
+var port = 3000;
+
+app.post('/send-email', function (req, res) {
+    var transporter = nodeMailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: 'la.stepanovs@gmail.com',
+          pass: 'hswkbgnqjuqjvhdc'
+        }
+      });
+
+      console.log(req.body);
+      
+      var mailOptions = {
+        to: 'la.stepanovs@gmail.com',
+        replyTo:`${req.body.name} <${req.body.email}>`,
+        subject: req.body.badget,
+        text: req.body.description,
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.err(error);
+          res.end(error);
+        } else {
+          console.info('Email sent: ' + info.response);
+          res.end(info.response);
+        }
+      });
+});
+      
+app.listen(port, function(){
+    console.log('Server is running at port: ',port);
+});
